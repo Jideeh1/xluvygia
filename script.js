55c0be548f802e039723f8c37b885418337f1adb
John Daniel Absalud <keonagjd@gmail.com>
2025-05-16 14:15:24 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var messageOne = "If you're here to scrutinize the logic of my code, reconsider before your head starts hurting.\n\n";
+var messageTwo = "It'd be a big mistake trying to read my HTML and JavaScript. My css however... I'm proud of the way I arranged it.\n\n";
+var messageThree = "Want to take a peek? Go to Sources.\n\n";
+var messageFour = "Seriously though, if you just want to take some JS functions, or see how I styled my elements, the code is open-source in my github.\n\n";
+var messageFive = "find it on: https://github.com/Jideeh1\n\n";
+var messageSix = "Just don't start complaining when you see how ass I write code. ーｗー\n\n";
+var signature = "Yours Truly, Jideeh\n\n";
+
+console.log(messageOne + messageTwo + messageThree + messageFour + messageFive + messageSix + signature);
